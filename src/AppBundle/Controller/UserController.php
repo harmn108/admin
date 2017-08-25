427289ff75f64168f8d199b2bb51978e7f597608
haykgrigoryan <hayk.g@blockchainrndhub.com>
2017-08-25 20:43:37 +0400

    form validation and error message styles

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,7 +5,9 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -14,7 +16,6 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserController extends Controller
@@ -61,15 +62,27 @@ class UserController extends Controller
 
         $form = $this->createFormBuilder()
             ->setMethod('POST')
-            ->add('email', TextType::class, ['attr'   =>  array('class'   => 'form-control m-bottom-10')])
-            ->add('password', PasswordType::class, ['attr'   =>  array('class'   => 'form-control m-bottom-10')])
+            ->add('email', EmailType::class, [
+                'attr'   =>  [
+                    'class'   => 'form-control m-bottom-10', 
+                    'required' => 'required',
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'attr'   =>  [
+                    'class'   => 'form-control m-bottom-10', 
+                    'required' => 'required'
+                ]
+            ])
             ->add('Sign In', SubmitType::class, ['attr'   =>  array('class'   => 'btn btn-primary btn-block m-top-10', 'label' => 'SignIn')])
             ->getForm();
 
+        $errors = [];
+
         $form->handleRequest($request);
 
+
         if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
             $data = $form->getData();
 
             $email = $data['email'];
@@ -84,33 +97,34 @@ class UserController extends Controller
             $user = $repository->findOneBy(['email' => $email, 'password' => md5($password)]);
 
             if ($user === null) {
-                //show error
-                return $this->redirect('login');
+                $errors[] = 'Invalid Email or Password';
             }
-
-            //  generate token
-            $token = md5(random_bytes(16));
-            $tokenEncoded = md5($token);
-            $findUser = $repository->findOneBy(['token' => $tokenEncoded]);
-
-            //  check if token is unique else generate new one
-            while ($findUser !== null) {
+            else{
+                //  generate token
                 $token = md5(random_bytes(16));
                 $tokenEncoded = md5($token);
                 $findUser = $repository->findOneBy(['token' => $tokenEncoded]);
+
+                //  check if token is unique else generate new one
+                while ($findUser !== null) {
+                    $token = md5(random_bytes(16));
+                    $tokenEncoded = md5($token);
+                    $findUser = $repository->findOneBy(['token' => $tokenEncoded]);
+                }
+
+                $user->setToken($tokenEncoded);
+                $em->persist($user);
+                $em->flush();
+
+                setcookie('X-API-TOKEN', $token, time()+3600, "/");
+
+                return $this->redirect('admin');
             }
-
-            $user->setToken($tokenEncoded);
-            $em->persist($user);
-            $em->flush();
-
-            setcookie('X-API-TOKEN', $token, time()+3600, "/");
-
-            return $this->redirect('admin');
         }
 
         return $this->render('user/login.html.twig', array(
             'form' => $form->createView(),
+            'errors' => $errors
         ));
     }
     
