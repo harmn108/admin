0c234c74e40ae2257508ecf4125606e1f577f881
Harutyun Mnatsakanyan <harmn90@gmail.com>
2017-08-25 16:52:59 +0400

    login action with token validation and regeneratin and cookie actions

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -56,10 +57,9 @@ class UserController extends Controller
         $form = $this->createFormBuilder()
             ->setAction('login_check')
             ->setMethod('POST')
-            ->setAttributes(['class' => 'form-group'])
-            ->add('email', TextType::class, ['attr'   =>  array('class'   => 'form-control')])
-            ->add('password', PasswordType::class, ['attr'   =>  array('class'   => 'form-control')])
-            ->add('Sign In', SubmitType::class, ['attr'   =>  array('class'   => 'btn btn-primary btn-block', 'label' => 'SignIn')])
+            ->add('email', TextType::class, ['attr'   =>  array('class'   => 'form-control m-bottom-10')])
+            ->add('password', PasswordType::class, ['attr'   =>  array('class'   => 'form-control m-bottom-10')])
+            ->add('Sign In', SubmitType::class, ['attr'   =>  array('class'   => 'btn btn-primary btn-block m-top-10', 'label' => 'SignIn')])
             ->getForm();
 
         return $this->render('user/login.html.twig', array(
@@ -82,6 +82,47 @@ class UserController extends Controller
      */
     public function loginCheckAction(Request $request)
     {
-        return new JsonResponse($request->getContent());
+        $data = $request->request->get('form');
+        $email = $data['email'];
+        $password = $data['password'];
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository(User::class);
+
+        /**
+         * @var $user User
+         */
+        $user = $repository->findOneBy(['email' => $email, 'password' => md5($password)]);
+
+        if ($user === null) {
+            return new JsonResponse(Response::$statusTexts[Response::HTTP_NOT_FOUND], Response::HTTP_NOT_FOUND);
+        }
+
+        //  generate token
+        $token = md5(random_bytes(16));
+        $tokenEncoded = md5($token);
+        $findUser = $repository->findOneBy(['token' => $tokenEncoded]);
+
+        //  check if token is unique else generate new one
+        while ($findUser !== null) {
+            $token = md5(random_bytes(16));
+            $tokenEncoded = md5($token);
+            $findUser = $repository->findOneBy(['token' => $tokenEncoded]);
+        }
+
+        $user->setToken($tokenEncoded);
+        $em->persist($user);
+        $em->flush();
+
+        setcookie('X-API-TOKEN', $token, time()+3600, "/");
+
+        return new JsonResponse([
+            'token' => $token,
+            'email' => $user->getEmail(),
+            'roleId'=>$user->getRoleId()->getId(),
+            'roleName'=>$user->getRoleId()->getName(),
+            'firstName' => $user->getFirstName(),
+            'lastName'=>$user->getLastName()
+        ]);
     }
 }
